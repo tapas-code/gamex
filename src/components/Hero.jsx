ec964c25d18e2b95e6fe44f2b92ad34586bbd89a
tapas-code <tapas.code247@gmail.com>
2025-03-23 10:45:06 +0530

    Create Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,85 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+  const totalVideos = 3;
+  const nextVideoRef = useRef(null);
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
   return (
-    <div className='relative h-dvh w-screen overflow-x-hidden'>
+    <div className="relative h-dvh w-screen overflow-x-hidden">
       {/* Hero Video Container  */}
-      <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-x-hidden rounded-lg bg-blue-75'>
-        <div className='mask-clip-path'></div>
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-x-hidden rounded-lg bg-blue-75"
+      >
+        <div>
+          <div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div
+              onClick={handleMiniVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100 "
+            >
+              {/* FOREGROUND VIDEO SWITCH  */}
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(upcomingVideoIndex)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
+          </div>
+          {/* BACKGROUND VIDEO PLAYER  */}
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+        <h1 className=" hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+            G<b className="special-font">a</b>ming
+        </h1>
+        <div className="absolute left-0 top-0 z-40 size-full">
+            <div className="mt-24 px-5 sm:px-10">
+                <h1 className=" hero-heading text-blue-100">redefi<b className="special-font">n</b>e</h1>
+                <p className="mb-5 max-w-64 font-robert-regular text-blue-100">Enter the Metagame Layer <br/> Unleash the Play Economy</p>
+            </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
